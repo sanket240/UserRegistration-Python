c6b3e1c697a5811d6069e92fe426fe4c020bb3b5
sanket2498 <sanketdulange24@gmail.com>
2021-01-11 09:54:13 +0530

    [sanket] Added : To check valid email

--- test_py.py
+++ test_py.py
@@ -17,3 +17,11 @@ def test_last_name_when_proper_should_return_happy():
 
 def test_last_name_when_improper_should_return_sad():
     assert user.last_name_validation("Du") == "Sad"
+
+
+def test_email_when_proper_should_return_happy():
+    assert user.email_validation("sanketdulange24@gmail.com") == "Happy"
+
+
+def test_email_when_improper_should_return_sad():
+    assert user.email_validation("sank.gmail.com") == "Sad"
--- user_registration.py
+++ user_registration.py
@@ -5,6 +5,7 @@ class UserRegistration:
     def __init__(self):
         self.first_name = "^[A-Z][a-zA-Z]{2,}$"
         self.last_name = "^[A-Z][a-zA-Z]{2,}$"
+        self.email = "^[a-zA-z]{3}[0-9a-zA-Z\\.\\_\\-\\+]*@[a-z]*\\.(co|com.au|in|net|in|com.com|com|)$"
 
     def first_name_validation(self, first_name_input):
         if re.match(self.first_name, first_name_input):
@@ -20,4 +21,12 @@ class UserRegistration:
             return "Happy"
         else:
             print("Invalid Last Name ")
+            return "Sad"
+
+    def email_validation(self, email_input):
+        if re.match(self.email, email_input):
+            print("Valid Email")
+            return "Happy"
+        else:
+            print("Invalid Email ")
             return "Sad"
