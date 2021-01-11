61bf4139f89749d38cda6458e81f84990c59001c
sanket2498 <sanketdulange24@gmail.com>
2021-01-11 10:01:17 +0530

    [sanket] Added : To check valid phone number

--- user_registration.py
+++ user_registration.py
@@ -6,6 +6,7 @@ class UserRegistration:
         self.first_name = "^[A-Z][a-zA-Z]{2,}$"
         self.last_name = "^[A-Z][a-zA-Z]{2,}$"
         self.email = "^[a-zA-z]{3}[0-9a-zA-Z\\.\\_\\-\\+]*@[a-z]*\\.(co|com.au|in|net|in|com.com|com|)$"
+        self.phone = "^[0-9]{2} [0-9]{10}$"
 
     def first_name_validation(self, first_name_input):
         if re.match(self.first_name, first_name_input):
@@ -30,3 +31,13 @@ class UserRegistration:
         else:
             print("Invalid Email ")
             return "Sad"
+
+    def phone_number_validation(self, phone_number_input):
+        if re.match(self.phone, phone_number_input):
+            print("Valid Phone")
+            return "Happy"
+        else:
+            print("Invalid Phone ")
+            return "Sad"
+
+
